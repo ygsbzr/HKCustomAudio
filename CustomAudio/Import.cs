1e43ea9c4e7f01d2e26a1b24175a78e8e14fb850
Mulhim <mulhima003@gmail.com>
2023-02-15 17:35:37 -0500

    Update Custom Audio
    - use wavLib
    - hook AudioSource Plays's instead of PMFSM Plays
    - hook scene load to replace audios that are play on awake
    - make sure hooks dont hook twice

--- CustomAudio/Import.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System.IO;
-global using UnityEngine;
-global using System.Text;
-global using HutongGames.PlayMaker;
-global using HutongGames.PlayMaker.Actions;
-global using USceneManager = UnityEngine.SceneManagement.SceneManager;
-global using UnityEngine.SceneManagement;
-global using UObject=UnityEngine.Object;
-global using System.Reflection;
